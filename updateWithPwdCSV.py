76a442d2c435846bc89d3e4ad533209dfc761267
Fabian Geiselhart <me@f4814n.de>
2018-11-05 19:52:53 +0100

    Fix user deletion

--- updateWithPwdCSV.py
+++ updateWithPwdCSV.py
@@ -25,7 +25,8 @@ def randomword(length):
 def mkPasswd(r):
     t = open('new-pws.csv', 'a')
     pw = randomword(6)
-    out = ';'.join([row[4],row[2],row[1],row[0],pw]) + '\n'
+    # Username, Last name, FirstName, pw, grade
+    out = ';'.join([row[0], row[2], row[1], pw, row[4]]) + '\n'
     t.write(out)
     return pw
 
@@ -39,41 +40,45 @@ grades = toDict(session.get('http://' + sys.argv[1] + '/api/user/grade/info').js
 users = session.post('http://' + sys.argv[1] + '/api/user/info', json={}).json()
 
 currUsers = []
+stage = int(sys.argv[4])
 with open('new-list.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
-    for row in readCSV:
-        username = row[0].lower()
-        currUsers.append(username)
-        if hasUser(users, username):
-            print("Updating user: " + username)
-            data = {'idUsername': username,
-                    'password': mkPasswd(row),
-                    'gradeId': grades[row[4]]}
-            q = session.post("http://" + sys.argv[1] + '/api/user/update',
-                             json=data).text
-            print(q)
-        else:
-            data = {'firstName': row[1],
-                    'lastName': row[2],
-                    'gradeId': grades[row[4]],
-                    'username': row[0],
-                    'password': mkPasswd(row),
-                    'role': {'citizen': True,
-                             'representative': False,
-                             'tech': False,
-                             'admin': False,
-                             'teacher': None,
-                             'customs': False
-                             }
-                    }
-            q = session.post('http://' + sys.argv[1] + '/api/user/add',
-                             json=data).text
-            print("Adding user: " + username)
-            print(q)
+    if stage == 1:
+        for row in readCSV:
+            username = row[0].lower()
+            if hasUser(users, username):
+                print("Updating user: " + username)
+                data = {'idUsername': username,
+                        'password': mkPasswd(row),
+                        'gradeId': grades[row[4]]}
+                q = session.post("http://" + sys.argv[1] + '/api/user/update',
+                                json=data).text
+                print(q)
+            else:
+                data = {'firstName': row[1],
+                        'lastName': row[2],
+                        'gradeId': grades[row[4]],
+                        'username': row[0],
+                        'password': mkPasswd(row),
+                        'role': {'citizen': True,
+                                'representative': False,
+                                'tech': False,
+                                'admin': False,
+                                'teacher': None,
+                                'customs': False
+                                }
+                        }
+                q = session.post('http://' + sys.argv[1] + '/api/user/add',
+                                json=data).text
+                print("Adding user: " + username)
+                print(q)
 
-    for u in users:
-        if u['username'] not in currUsers:
-            print("Deleting user: " + u['username'])
-            q = session.post('http://' + sys.argv[1] + '/api/user/remove',
-                             json={'idUsername': u['username']}).text
-            print(q)
+    if stage == 2:
+        for row in readCSV:
+                currUsers.append(row[0].lower())
+        for u in users:
+            if u['username'] not in currUsers:
+                print("Deleting user: " + u['username'])
+                q = session.post('http://' + sys.argv[1] + '/api/user/remove',
+                                json={'idUsername': u['username']}).text
+                print(q)
